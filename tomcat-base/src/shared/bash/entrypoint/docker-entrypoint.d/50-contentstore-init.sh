22f7d9102476f8820b52b998a1503f49a02ffa02
Thijs Lemmens <thijs.lemmens@xenit.eu>
2022-08-31 13:43:52 +0200

    DOCKER-414 Default dir.root to /opt/alfresco/alf_data and set permissions when empty

---
+++ tomcat-base/src/shared/bash/entrypoint/docker-entrypoint.d/50-contentstore-init.sh
@@ -0,0 +1,18 @@
+mkdir -p /opt/alfresco/alf_data
+user="tomcat"
+# Check existence of alf_data/contentstore
+# -> if dir exists, check if the permissions are incorrect
+# --> if permissions are incorrect, exit with error code
+# --> else if correct, do nothing
+# -> else if not exists, chown only alf_data
+if [[ -e /opt/alfresco/alf_data/contentstore && -e /opt/alfresco/alf_data/contentstore.deleted ]]; then
+  if [[ ($(stat -c %U /opt/alfresco/alf_data/contentstore) != "$user" && $(stat -c %a /opt/alfresco/alf_data/contentstore) -lt 766)\
+   || ($(stat -c %U /opt/alfresco/alf_data/contentstore.deleted) != "$user" && $(stat -c %a /opt/alfresco/alf_data/contentstore.deleted) -lt 766) ]]; then
+    # custom exit code for debug to this script
+    echo "Contentstores exists within /opt/alfresco/alf_data , but do not have correct ownership/permission to run alfresco. Exiting with code 64."
+    exit 64
+  fi
+else
+  echo "No contentstore in alf_data ; assuming dev/test environment, chowning alf_data to tomcat user."
+  chown $user:$user /opt/alfresco/alf_data
+fi
