ee0b8f3e5a55660bdf5ee5fc252fcc36ea82bcbf
Hechmi Dammak <hechmi.dammak@xenit.eu>
2023-05-25 16:13:56 +0100

    XM2C-105 docker-alfresco fix init script error for build date not parsed as int

--- tomcat-base/src/shared/main/bash/entrypoint/docker-entrypoint.d/89-check-validity.sh
+++ tomcat-base/src/shared/main/bash/entrypoint/docker-entrypoint.d/89-check-validity.sh
@@ -5,7 +5,8 @@ if [ -z "${BUILD_DATE}" ]
 then
     1>&2 echo "$(tput -Txterm setaf 3)"'{ "timestamp" : '"$(date -d @$currentDate +"%s")"' ,"severity" : "WARN", "type" : "application","component" : "docker-entrypoint" ,"fullMessage" : "Unable to determine image age: BUILD_DATE is not set"}'"$(tput -Txterm sgr0)"
 else
-    timeelapsed=`expr ${currentDate} - ${BUILD_DATE}`
+    buildDate= printf "%.0f\n" "$BUILD_DATE"
+    timeelapsed=`expr ${currentDate} - ${buildDate}`
     if [ $timeelapsed -ge 2592000 ] # 30 days
     then
 	1>&2 echo "$(tput -Txterm setaf 3)"'{ "timestamp" : '"$(date -d @$currentDate +"%s")"' ,"severity" : "WARN", "type" : "application","component" : "docker-entrypoint" , "fullMessage" : "This is an old image: BUILD_DATE='"$(date -d @$BUILD_DATE +"%Y-%m-%d %H:%M:%S")"' and currentDate='"$(date -d @$currentDate +"%Y-%m-%d %H:%M:%S")"'"}'"$(tput -Txterm sgr0)"
