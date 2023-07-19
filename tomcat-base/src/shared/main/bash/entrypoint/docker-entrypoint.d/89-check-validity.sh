14d63370e42c003cc7e83931fb17bfdb3ebd04f9
Hechmi Dammak <hechmi.dammak@xenit.eu>
2023-07-19 15:04:16 +0100

    XM2C-105 docker-alfresco fix init script error for build date not parsed as int

--- tomcat-base/src/shared/main/bash/entrypoint/docker-entrypoint.d/89-check-validity.sh
+++ tomcat-base/src/shared/main/bash/entrypoint/docker-entrypoint.d/89-check-validity.sh
@@ -5,7 +5,7 @@ if [ -z "${BUILD_DATE}" ]
 then
     1>&2 echo "$(tput -Txterm setaf 3)"'{ "timestamp" : '"$(date -d @$currentDate +"%s")"' ,"severity" : "WARN", "type" : "application","component" : "docker-entrypoint" ,"fullMessage" : "Unable to determine image age: BUILD_DATE is not set"}'"$(tput -Txterm sgr0)"
 else
-    buildDate= printf "%.0f\n" "$BUILD_DATE"
+    printf -v buildDate "%.0f\n" "$BUILD_DATE"
     timeelapsed=`expr ${currentDate} - ${buildDate}`
     if [ $timeelapsed -ge 2592000 ] # 30 days
     then
