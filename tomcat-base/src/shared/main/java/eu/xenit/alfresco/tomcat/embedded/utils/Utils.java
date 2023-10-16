61decc069117d18cb6987b496a79cad3e2e4b2e5
Hechmi Dammak <hechmi.dammak@xenit.eu>
2023-10-16 16:55:48 +0100

    DOCKER-439 add check to find files log4j.properties and log4j2.properties for alfresco

--- tomcat-base/src/shared/main/java/eu/xenit/alfresco/tomcat/embedded/utils/Utils.java
+++ tomcat-base/src/shared/main/java/eu/xenit/alfresco/tomcat/embedded/utils/Utils.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 
 public class Utils {
@@ -22,11 +25,8 @@ public class Utils {
     }
 
     public static void redirectLog4j(Path webappPath, Path destinationPath) {
-        Path log4JPropertiesPath = webappPath.resolve("WEB-INF/classes/log4j.properties");
-        if (!Files.exists(log4JPropertiesPath)) {
-            LOG.warning("Log4j file doesn't exist under path " + log4JPropertiesPath);
-            return;
-        }
+        Path log4JPropertiesPath = getLog4JPropertiesPath(webappPath);
+        if (log4JPropertiesPath == null) return;
         Properties properties = new Properties();
         try {
             try (var reader = Files.newBufferedReader(log4JPropertiesPath)) {
@@ -46,5 +46,21 @@ public class Utils {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Path getLog4JPropertiesPath(Path webappPath) {
+        List<String> log4jPathToTry = List.of("WEB-INF/classes/log4j.properties",
+                "WEB-INF/classes/log4j2.properties");
+        List<Path> log4jPaths = new ArrayList<>();
+        for (String path : log4jPathToTry) {
+            Path log4JPropertiesPath = webappPath.resolve(path);
+            log4jPaths.add(log4JPropertiesPath);
+            if (Files.exists(log4JPropertiesPath)) {
+                return log4JPropertiesPath;
+            }
+        }
+        LOG.warning("Log4j file doesn't exist under paths : " +
+                log4jPaths.stream().map(Path::toString).collect(Collectors.joining(" ,")));
+        return null;
     }
 }
