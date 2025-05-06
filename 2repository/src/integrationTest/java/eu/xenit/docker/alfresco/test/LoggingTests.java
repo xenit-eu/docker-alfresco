46827e4708f27d19fb4e369ee81ca51e29c22d76
Tim-Lukas Blom <tim-lukas.blom@xenit.eu>
2025-05-06 08:09:20 +0200

    DOCKER-464 Loggingtest using Dockerclient
    Signed-off-by: Tim-Lukas Blom <tim-lukas.blom@xenit.eu>

--- 2repository/src/integrationTest/java/eu/xenit/docker/alfresco/test/LoggingTests.java
+++ 2repository/src/integrationTest/java/eu/xenit/docker/alfresco/test/LoggingTests.java
@@ -2,7 +2,9 @@ package eu.xenit.docker.alfresco.test;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.ContainerConfig;
 import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
@@ -10,14 +12,20 @@ import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoggingTests {
 
+    private static final Logger log = LoggerFactory.getLogger(LoggingTests.class);
     final Set<String> REQ_COMMON_LOGGING_FIELDS = Set.of("timestamp", "type");
 
     final Set<String> REQ_APPLICATION_LOGGING_FIELDS = union(
@@ -72,7 +80,7 @@ public class LoggingTests {
         return true;
     }
 
-    private static String getContainerLogs(DockerClient dockerClient, Container container)
+    private static String getContainerLogs(DockerClient dockerClient, String containerId)
             throws InterruptedException {
         StringBuilder logs = new StringBuilder();
         ResultCallback.Adapter<Frame> callback = new ResultCallback.Adapter<>() {
@@ -81,23 +89,69 @@ public class LoggingTests {
             }
         };
         // Might need to limit the amount of log lines retrieved if logs are too big using withTail(n)
-        dockerClient.logContainerCmd(container.getId()).withStdOut(true).withStdErr(true).exec(callback)
+        dockerClient.logContainerCmd(containerId).withStdOut(true).withStdErr(true).exec(callback)
                 .awaitCompletion();
         return logs.toString();
     }
 
     @Test
     public void testJsonLogging() throws IOException, InterruptedException {
-        assert "true".equals(System.getenv().get("JSON_LOGGING"));
 
         final String containerName = "alfresco";
         try (DockerClient dockerClient = createDockerClient()) {
             List<Container> containers = dockerClient.listContainersCmd().withStatusFilter(List.of("running")).exec();
             Container container = findContainerByName(containerName, containers);
             assert container != null;
-            String logs = getContainerLogs(dockerClient, container);
+            System.out.println(
+                    Arrays.toString(dockerClient.inspectContainerCmd(container.getId()).exec().getConfig().getEnv()));
+            String logs = getContainerLogs(dockerClient, container.getId());
             Assert.assertFalse(logs.isEmpty());
             Assert.assertTrue("Logs do not contain required fields or aren't json", isCorrectJsonLogs(logs));
+        }
+    }
+
+    @Test
+    public void testNonJsonLogging() throws IOException, InterruptedException {
+
+        final String containerName = "alfresco";
+        try (DockerClient dockerClient = createDockerClient()) {
+            List<Container> containers = dockerClient.listContainersCmd().withStatusFilter(List.of("running")).exec();
+            Container container = findContainerByName(containerName, containers);
+            assert container != null;
+            String containerId = container.getId();
+            System.out.println(
+                    Arrays.toString(dockerClient.inspectContainerCmd(containerId).exec().getConfig().getEnv()));
+            InspectContainerResponse inspectResp = dockerClient.inspectContainerCmd(containerId).exec();
+
+            // Change envVars
+            ContainerConfig config = inspectResp.getConfig();
+            List<String> envVars = new ArrayList<>(
+                    config.getEnv() != null ? List.of(config.getEnv()) : Collections.emptyList());
+            envVars.removeIf(s -> s.startsWith("JSON_LOGGING="));
+            envVars.add("JSON_LOGGING=false");
+
+            // Create new container
+            String newContainerId = dockerClient.createContainerCmd(config.getImage())
+                    .withName("no_json_container")
+                    .withEnv(envVars)
+                    .withCmd(config.getCmd())
+                    .withEntrypoint(config.getEntrypoint())
+                    .withHostConfig(inspectResp.getHostConfig())
+                    .exec()
+                    .getId();
+
+            // Start new container
+            dockerClient.startContainerCmd(newContainerId).exec();
+            Thread.sleep(10_000);
+            InspectContainerResponse newInspectResp = dockerClient.inspectContainerCmd(newContainerId).exec();
+
+            // Check logs of new container
+            String logs = getContainerLogs(dockerClient, newInspectResp.getId());
+            Assert.assertFalse(logs.isEmpty());
+            Assert.assertFalse("Logs should not be json logs", isCorrectJsonLogs(logs));
+            dockerClient.stopContainerCmd(newContainerId).withTimeout(5).exec();
+            dockerClient.removeContainerCmd(newContainerId).exec();
+            assert false;
         }
     }
 }
